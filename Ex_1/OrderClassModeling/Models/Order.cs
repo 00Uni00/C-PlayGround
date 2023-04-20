939ef0a587270e53d75dd665562d343a7922fbdd
Uni <86801127+00Uni00@users.noreply.github.com>
2023-04-20 20:48:11 +0200

    Add files via upload

---
+++ Ex_1/OrderClassModeling/Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderClassModeling.Models
+{
+    class Order
+    {
+        public int Id { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal TaxRate { get; } = 0.23M;
+        public decimal Balance 
+        {
+            get 
+            {
+                return Price * (1 + TaxRate); 
+            } 
+        }
+    
+        public Order(int id, decimal price) 
+        {
+            if (price <= 0) 
+            {
+                throw new Exception("Price must be greater then zero.");
+            }
+            Id = id;
+            Price = price;  
+        }
+    }
+}
---
+++ Ex_1/OrderClassModeling/Program.cs
@@ -0,0 +1,17 @@
+﻿using OrderClassModeling.Models;
+
+namespace OrderClassModeling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Order Table = new Order(1, 200);
+            User Uni = new User("unimail@gmail.com", "Password123#$%");
+            Uni.AddFunds(300);
+            Console.WriteLine(Uni.Funds);
+            Uni.PurchaseOrder(Table);
+            Console.WriteLine(Uni.Funds);
+        }
+    }
+}
