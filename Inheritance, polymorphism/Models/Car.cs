c7909ab8be2f6a4a2465faf60b282ff9b9d6b03d
Uni <86801127+00Uni00@users.noreply.github.com>
2023-04-21 19:50:16 +0200

    Add files via upload

---
+++ Inheritance, polymorphism/Models/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Models
+{
+    public abstract class Car
+    {
+        public double Speed { get; protected set; } = 20;
+        public bool Engine { get; protected set; } = false;
+
+        public void Start() 
+        {
+            Engine = true;
+            Console.WriteLine("Engine On.");
+        }
+        public void Stop() 
+        {
+            Engine = false;
+            Console.WriteLine("Engine Off.");
+        }
+        public virtual void Accelerate()
+        {
+            Console.WriteLine("Accelerating");
+            Console.WriteLine($"{Speed}km/h");
+        }
+    }
+    public class Truck : Car
+    {
+        public override void Accelerate()
+        {
+            base.Accelerate();
+            Speed += 5;
+            Console.WriteLine($"After accelerate {Speed} km/h");
+        }
+    }
+    public class SportCar : Car 
+    {
+        public override void Accelerate()
+        {
+            base.Accelerate();
+            Speed += 20;
+            Console.WriteLine($"After accelerate {Speed} km/h");
+        }
+    }
+
+    public class Race 
+    {
+        public void StartOfRace() 
+        {
+            Truck truck = new Truck();
+            SportCar sportCar = new SportCar();
+
+            List<Car> cars = new List<Car>
+            {
+                truck,sportCar  
+            };
+
+            foreach (var car in cars) 
+            {
+                car.Start();
+                car.Accelerate();
+            }
+        }
+    }
+}
---
+++ Inheritance, polymorphism/Program.cs
@@ -0,0 +1,13 @@
+﻿using OOP.Models;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Race race = new Race();
+            race.StartOfRace();
+        }
+    }
+}
