93fad5db000ec9e628eb9a39424aa75c36efb97f
Uni <86801127+00Uni00@users.noreply.github.com>
2023-04-24 18:13:41 +0200

    Add files via upload

---
+++ Inheritance, polymorphism/Models/Interfaces.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderClassModeling.Models;
+
+namespace OrderClassModeling.Models
+{
+    public interface IEmailSender 
+    {
+        void SendEmail(string reciver, string topic, string text);   
+    }
+    public interface IDatabase 
+    {
+        bool IsConnected { get; }
+        void Connect();
+        User GetUser();
+        Order GetOrder();
+        void SaveChanges();
+    }
+    public class Database : IDatabase
+    {
+        public bool IsConnected => throw new NotImplementedException();
+
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EmailSender : IEmailSender
+    {
+        public void SendEmail(string reciver, string topic, string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SuceedPurchase 
+    {
+        private readonly Database _database;
+        private readonly EmailSender _emailSender;
+        public SuceedPurchase(Database database, EmailSender emailSender)
+        {
+            _database = database;
+            _emailSender = emailSender;
+        }
+        public void ProcessOrder(string email, int orderId) 
+        {
+            User user = _database.GetUser();
+            Order order = _database.GetOrder();
+            user.PurchaseOrder(order);
+            _database.SaveChanges();
+            _emailSender.SendEmail(email,$"Order {orderId}" ,"Order complete.We will send you packet soon.");
+        }
+    }
+
+}
---
+++ Inheritance, polymorphism/Models/User.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderClassModeling.Models
+{
+    public class User
+    {
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Funds { get; private set; } = 0;
+
+        public User(string email, string password)
+        {
+            SetEmail(email);
+            SetPassowrd(password);
+        }
+
+        public void SetEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new Exception("Email is incorrect.");
+            }
+            Email = email;
+        }
+        public void SetPassowrd(string password)
+        {
+            if(string.IsNullOrWhiteSpace(password)) 
+            {
+                throw new Exception("Password is incorrect.");
+            }
+            Password = password;
+        }
+
+        public void SetFirstName(string firstName) 
+        {
+            if(string.IsNullOrWhiteSpace(firstName)) 
+            {
+                throw new Exception("First Name is incorrect.");
+            }
+            FirstName = firstName;
+        }
+
+        public void SetLastName(string lastName) 
+        {
+            if(string.IsNullOrWhiteSpace(lastName)) 
+            {
+                throw new Exception("Last Name is incorrect.");
+            }
+            LastName = lastName;    
+        }
+
+        public void SetAge(int age) 
+        {
+            if(age < 18) 
+            {
+                throw new Exception("You must be an adult");
+            }
+            Age = age;
+        }
+        public void AddFunds(decimal funds)
+        {
+            if (funds <= 0)
+            {
+                throw new Exception("You can not add it to your balnace");
+            }
+            Funds += funds;
+        }
+        public void PurchaseOrder(Order order) 
+        {
+            decimal orderPrice = order.Balance;
+            if (Funds - orderPrice < 0) 
+            {
+                throw new Exception("You do not have enough money.");
+            }
+            Funds -= orderPrice;
+        }
+    }
+}
